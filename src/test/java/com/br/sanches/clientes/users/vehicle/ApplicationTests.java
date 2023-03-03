3f3eaec531c3dc64d0b339c02d259136bbcd7d3d
marcosvinicius <marcossanches710@gmail.com>
2023-03-03 14:51:04 -0300

    Terminando de escrever os testes do CRUD,

--- src/test/java/com/br/sanches/clientes/users/vehicle/ApplicationTests.java
+++ src/test/java/com/br/sanches/clientes/users/vehicle/ApplicationTests.java
@@ -12,6 +12,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,73 +21,127 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.testng.AssertJUnit.assertFalse;
+import static org.testng.AssertJUnit.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
 
-	private TestRestTemplate restTemplate;
+    private TestRestTemplate restTemplate;
 
-	private UserRepository userRepository;
+    private UserRepository userRepository;
 
-	private CarRepository carRepository;
+    private CarRepository carRepository;
 
-	private ConvertionsTest convertionsTest;
-	@Autowired
-	ApplicationTests(TestRestTemplate restTemplate, UserRepository userRepository, CarRepository carRepository, ConvertionsTest convertionsTest) {
-		this.restTemplate = restTemplate;
-		this.userRepository = userRepository;
-		this.carRepository = carRepository;
-		this.convertionsTest = convertionsTest;
-	}
+    private ConvertionsTest convertionsTest;
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    ApplicationTests(TestRestTemplate restTemplate, UserRepository userRepository, CarRepository carRepository, ConvertionsTest convertionsTest) {
+        this.restTemplate = restTemplate;
+        this.userRepository = userRepository;
+        this.carRepository = carRepository;
+        this.convertionsTest = convertionsTest;
+    }
 
-	@Test
-	public void sholdRegisterNewUser() throws PreconditionFailedException {
+    @Test
+    void contextLoads() {
+    }
 
-		UserRequest userRequest = convertionsTest.instantiatingANewUserAndVehicleForTheTest();
+    @Test
+    public void sholdRegisterNewUser() throws PreconditionFailedException {
 
-		ResponseEntity<UserResponse> response = restTemplate.postForEntity(
-				"http://localhost:9060/clientes/users/register", userRequest, UserResponse.class);
+        UserRequest userRequest = convertionsTest.instantiatingANewUserAndVehicleForTheTest();
 
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        ResponseEntity<UserResponse> response = restTemplate.postForEntity(
+                "http://localhost:9060/clientes/users/register", userRequest, UserResponse.class);
 
-		UserResponse userResponse = response.getBody();
-		assertThat(userResponse.getUserName()).isEqualTo(userRequest.getUserName());
-		assertThat(userResponse.getName()).isEqualTo(userRequest.getName());
-		assertThat(userResponse.getCarResponse().getLicensePlate()).isEqualTo(userRequest.getCarRequest().getLicensePlate().toUpperCase());
-		assertThat(userResponse.getCarResponse().getVehicleModel()).isEqualTo(userRequest.getCarRequest().getVehicleModel().toUpperCase());
-		assertThat(userResponse.getCarResponse().getCountry()).isEqualTo(userRequest.getCarRequest().getCountry());
-		assertThat(userResponse.getCarResponse().getState()).isEqualTo(userRequest.getCarRequest().getState());
-		assertThat(userResponse.getCarResponse().getCity()).isEqualTo(userRequest.getCarRequest().getCity());
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
-		Optional<UserEntity> savedUser = userRepository.findByUserName(userResponse.getUserName());
-		assertThat(savedUser.isPresent()).isTrue();
-		Optional<EntityCars> savedCar = carRepository.findByLicensePlate(userRequest.getCarRequest().getLicensePlate());
-		assertThat(savedCar.isPresent()).isTrue();
-	}
+        UserResponse userResponse = response.getBody();
+        assertThat(userResponse.getUserName()).isEqualTo(userRequest.getUserName());
+        assertThat(userResponse.getName()).isEqualTo(userRequest.getName());
+        assertThat(userResponse.getCarResponse().getLicensePlate()).isEqualTo(userRequest.getCarRequest().getLicensePlate().toUpperCase());
+        assertThat(userResponse.getCarResponse().getVehicleModel()).isEqualTo(userRequest.getCarRequest().getVehicleModel().toUpperCase());
+        assertThat(userResponse.getCarResponse().getCountry()).isEqualTo(userRequest.getCarRequest().getCountry());
+        assertThat(userResponse.getCarResponse().getState()).isEqualTo(userRequest.getCarRequest().getState());
+        assertThat(userResponse.getCarResponse().getCity()).isEqualTo(userRequest.getCarRequest().getCity());
 
-	@Test
-	public void sholdSearchByUserId()throws PreconditionFailedException{
+        Optional<UserEntity> savedUser = userRepository.findByUserName(userResponse.getUserName());
+        assertThat(savedUser.isPresent()).isTrue();
+        Optional<EntityCars> savedCar = carRepository.findByLicensePlate(userRequest.getCarRequest().getLicensePlate());
+        assertThat(savedCar.isPresent()).isTrue();
+    }
 
-		ResponseEntity<UserResponse> response = this.restTemplate.getForEntity(
-				"http://localhost:9060/clientes/users/busca/1",
-				UserResponse.class);
+    @Test
+    public void sholdSearchByUserId() throws PreconditionFailedException {
 
+        ResponseEntity<UserResponse> response = this.restTemplate.getForEntity(
+                "http://localhost:9060/clientes/users/busca/1",
+                UserResponse.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        UserResponse userResponse = response.getBody();
+        assertThat(userResponse).isNotNull();
+
+        assertThat(userResponse.getIdUser()).isEqualTo(1L);
+        assertThat(userResponse.getUserName()).isEqualTo("Markin_Dev10");
+        assertThat(userResponse.getName()).isEqualTo("Marcos Vinicius Campos");
+        assertThat(userResponse.getCarResponse().getLicensePlate().toUpperCase()).isEqualTo("OLK1234");
+        assertThat(userResponse.getCarResponse().getVehicleModel().toUpperCase()).isEqualTo("HONDA CIVIC LXS");
+        assertThat(userResponse.getCarResponse().getCountry()).isEqualTo("Brasil");
+        assertThat(userResponse.getCarResponse().getState()).isEqualTo("São Paulo");
+        assertThat(userResponse.getCarResponse().getCity()).isEqualTo("Mirassol");
+    }
+
+    @Test
+    public void sholdUpdateUserAndCar() throws PreconditionFailedException {
+
+        UserRequest userRequest = convertionsTest.updateUserAndCarTest();
+
+        ResponseEntity<UserResponse> response = restTemplate.exchange(
+                "http://localhost:9060/clientes/users/altera-os-dados/1",
+                HttpMethod.PUT,
+                new HttpEntity<>(userRequest),
+                UserResponse.class);
+
+        assertThat(HttpStatus.OK).isEqualTo(response.getStatusCode());
+
+        UserResponse userResponse = response.getBody();
+        assertNotNull(userResponse);
+        assertThat(userResponse.getUserName()).isEqualTo(userRequest.getUserName());
+        assertThat(userResponse.getName()).isEqualTo(userRequest.getName());
+        assertNotNull(userResponse.getCarResponse());
+        assertThat(userResponse.getCarResponse().getLicensePlate()).isEqualTo(userRequest.getCarRequest().getLicensePlate().toUpperCase());
+        assertThat(userResponse.getCarResponse().getVehicleModel()).isEqualTo(userRequest.getCarRequest().getVehicleModel().toUpperCase());
+        assertThat(userResponse.getCarResponse().getCountry()).isEqualTo(userRequest.getCarRequest().getCountry());
+        assertThat(userResponse.getCarResponse().getState()).isEqualTo(userRequest.getCarRequest().getState());
+        assertThat(userResponse.getCarResponse().getCity()).isEqualTo(userRequest.getCarRequest().getCity());
+    }
+
+    @Test
+    public void sholdDeleteAUserAndVehicleByItsId() {
+
+        UserEntity userEntity = userRepository.findById(1L).orElse(null);
+        assertNotNull(userEntity);
+
+        EntityCars entityCars = carRepository.findById(1L).orElse(null);
+        assertNotNull(entityCars);
+
+        entityCars.setIdUser(userEntity);
+        carRepository.save(entityCars);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                "http://localhost:9060/clientes/users/deletar/1",
+                HttpMethod.DELETE,
+                null,
+                String.class,
+                userEntity.getIdUser()
+        );
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-		UserResponse userResponse = response.getBody();
-		assertThat(userResponse).isNotNull();
 
-		assertThat(userResponse.getIdUser()).isEqualTo(1L);
-		assertThat(userResponse.getUserName()).isEqualTo("Markin_Dev10");
-		assertThat(userResponse.getName()).isEqualTo("Marcos Vinicius Campos");
-		assertThat(userResponse.getCarResponse().getLicensePlate().toUpperCase()).isEqualTo("OLK1234");
-		assertThat(userResponse.getCarResponse().getVehicleModel().toUpperCase()).isEqualTo("HONDA CIVIC LXS");
-		assertThat(userResponse.getCarResponse().getCountry()).isEqualTo("Brasil");
-		assertThat(userResponse.getCarResponse().getState()).isEqualTo("São Paulo");
-		assertThat(userResponse.getCarResponse().getCity()).isEqualTo("Mirassol");
-	}
+        assertFalse(userRepository.findById(userEntity.getIdUser()).isPresent());
+        assertFalse(carRepository.findById(entityCars.getIdCar()).isPresent());
+
+    }
 }
--- src/test/java/com/br/sanches/clientes/users/vehicle/ConvertionsTest.java
+++ src/test/java/com/br/sanches/clientes/users/vehicle/ConvertionsTest.java
@@ -2,27 +2,80 @@ package com.br.sanches.clientes.users.vehicle;
 
 import com.br.sanches.clientes.users.vehicle.controller.request.CarRequest;
 import com.br.sanches.clientes.users.vehicle.controller.request.UserRequest;
+import com.br.sanches.clientes.users.vehicle.entity.EntityCars;
+import com.br.sanches.clientes.users.vehicle.entity.UserEntity;
+import com.br.sanches.clientes.users.vehicle.repository.CarRepository;
+import com.br.sanches.clientes.users.vehicle.repository.UserRepository;
+import com.br.sanches.clientes.users.vehicle.utils.ConverterUtil;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConvertionsTest {
 
+    private final UserRepository userRepository;
+    private final CarRepository carRepository;
+    @Autowired
+    public ConvertionsTest(UserRepository userRepository, CarRepository carRepository) {
+        this.userRepository = userRepository;
+        this.carRepository = carRepository;
+    }
+
     @NotNull
     public static UserRequest instantiatingANewUserAndVehicleForTheTest() {
         UserRequest userRequest = new UserRequest();
-        userRequest.setUserName("Markin_Dev20");
+        userRequest.setUserName("Markin_Dev10");
         userRequest.setName("Marcos Vinicius Campos");
         userRequest.setCpf("93610007864");
         userRequest.setPassword("Br12-Je11-Rb87");
 
         CarRequest carRequest = new CarRequest();
-        carRequest.setLicensePlate("efh2412");
+        carRequest.setLicensePlate("olk1234");
         carRequest.setVehicleModel("honda civic lxs");
         carRequest.setCountry("Brasil");
         carRequest.setState("São Paulo");
         carRequest.setCity("Mirassol");
         userRequest.setCarRequest(carRequest);
         return userRequest;
+    }
+
+    public static UserRequest updateUserAndCarTest() {
+
+        UserRequest userRequest = new UserRequest();
+        userRequest.setUserName("Markin_Dev");
+        userRequest.setName("Marcos Vinicius Campos");
+        userRequest.setCpf("93610007864");
+        userRequest.setPassword("Br12-Je11-Rb87");
+
+        CarRequest carRequest = new CarRequest();
+        carRequest.setLicensePlate("uir1234");
+        carRequest.setVehicleModel("bmw m4");
+        carRequest.setCountry("Brasil");
+        carRequest.setState("São Paulo");
+        carRequest.setCity("Mirassol");
+        userRequest.setCarRequest(carRequest);
+        return userRequest;
+    }
+
+    @NotNull
+    public UserEntity SaveUserEntityAndCarEntityToTestUpdateUserANdVehicle() {
+        UserEntity user = new UserEntity();
+        user.setName("Marcos Vinicius");
+        user.setUserName("NinjaFodão");
+        user.setCpf("39610007864");
+        user.setPassword("Br12-Je11-Rb87");
+        user.setDateRegister(ConverterUtil.nowTime());
+        userRepository.save(user);
+
+        EntityCars entityCars = new EntityCars();
+        entityCars.setVehicleModel("Ferrari f40");
+        entityCars.setLicensePlate("wre0632");
+        entityCars.setCountry("Brasil");
+        entityCars.setState("São Paulo");
+        entityCars.setCity("Mirassol");
+        entityCars.setIdUser(user);
+        carRepository.save(entityCars);
+        return user;
     }
 }
