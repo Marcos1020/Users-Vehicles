e6b2a0b50fe9bc5a0996d3840ff00a5a35a5a5c2
marcosvinicius <marcossanches710@gmail.com>
2023-05-02 10:28:59 -0300

    Adicionando a anotação @Builder na classe EntityCars.java para intanciar uma nova entidade atráves do lombok

--- src/main/java/com/br/sanches/clientes/users/vehicle/entity/EntityCars.java
+++ src/main/java/com/br/sanches/clientes/users/vehicle/entity/EntityCars.java
@@ -5,12 +5,10 @@ import com.br.sanches.clientes.users.vehicle.utils.DateAndTimeDeserializer;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import kafka.utils.Json;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.aspectj.bridge.IMessage;
-import org.springframework.data.annotation.ReadOnlyProperty;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
@@ -19,6 +17,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "CARS_TB")
 public class EntityCars {
