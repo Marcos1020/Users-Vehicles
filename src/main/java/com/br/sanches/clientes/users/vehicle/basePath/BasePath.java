bf11b848f60deb2b4c7d5d3c86eaa9f13f53602f
marcosvinicius <marcossanches710@gmail.com>
2023-05-02 10:24:53 -0300

    Identando a classe BasePath.java e implementando a URL

--- src/main/java/com/br/sanches/clientes/users/vehicle/basePath/BasePath.java
+++ src/main/java/com/br/sanches/clientes/users/vehicle/basePath/BasePath.java
@@ -5,11 +5,11 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class BasePath {
 
-    public final String BASE_PATH_URL = "clientes/users";
-    public final String BASE_PATH_REGISTER_NEW_USER = "register";
-    public final String BASE_PARAM_NAME = "search";
-    public final String BASE_PATH_ID_UPDATE = "altera-os-dados";
-    public final String BASE_PATH_ID_DELETE = "deletar/{id}";
+    public final String BASE_PATH_URL = "v1/api/clientes/users";
+    public final String BASE_PATH_REGISTER_NEW_USER = "/register";
+    public final String BASE_PARAM_NAME = "/search";
+    public final String BASE_PATH_ID_UPDATE = "/altera-os-dados";
+    public final String BASE_PATH_ID_DELETE = "/deletar/{id}";
     public final String BASE_PATH_LOGIN_USER = "/login-user";
     public final String BASE_PARAM_SEARCH_LICENSE_PLATE = "/search/license-plate";
     public final String BASE_PATH_ALTER_LICENSE_PLATE = "/alter/license-plate";
